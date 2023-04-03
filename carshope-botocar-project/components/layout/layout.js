c4e1fafe4f5264ad180699f0cf338a4a166700d6
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-03 17:13:01 +0330

    creating layout component

---
+++ carshope-botocar-project/components/layout/layout.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const layout = ({ children }) => {
+  return (
+    <>
+      <header>
+        <Link href="/">
+          <h2>Car shope</h2>
+          <p>Choss and buy your car</p>
+        </Link>
+      </header>
+      <div>{children}</div>
+      <footer>next toturial projects | Car shope &copy;</footer>
+    </>
+  );
+};
+
+export default layout;
