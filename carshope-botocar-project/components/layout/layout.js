8be9d749f1f3ae84105df023ea3078898bb06446
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-03 17:24:10 +0330

    adding styles to layout component

--- carshope-botocar-project/components/layout/layout.js
+++ carshope-botocar-project/components/layout/layout.js
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
+//styles
+import styles from './Layout.module.css'
+
 const Layout = ({ children }) => {
   return (
     <>
-      <header>
+      <header className={styles.header} >
         <Link href="/">
           <h2>Car shope</h2>
           <p>Choss and buy your car</p>
         </Link>
       </header>
-      <div>{children}</div>
-      <footer>next toturial projects | Car shope &copy;</footer>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>next toturial projects | Car shope &copy;</footer>
     </>
   );
 };
