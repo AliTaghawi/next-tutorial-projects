fb4cbe49f8ff04fe8f8108d7d874beaf0cb7c9c1
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-03 18:08:51 +0330

    adding styles to cars page temp

--- carshope-botocar-project/components/templates/Cars.js
+++ carshope-botocar-project/components/templates/Cars.js
@@ -1,7 +1,7 @@
-
+import styles from './Cars.module.css'
 const Cars = ({data}) => {
   return (
-    <div>
+    <div className={styles.container} >
       {data.map(car => <p key={car.id}>{car.name}</p>)}
     </div>
   );
