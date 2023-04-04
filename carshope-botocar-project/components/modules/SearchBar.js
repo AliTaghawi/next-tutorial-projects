606cf3d987bdcfde0fc5b223a9f0f67ae399423b
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-04 09:13:53 +0330

    <car shope project> creating search bar module and adding it in cars page

---
+++ carshope-botocar-project/components/modules/SearchBar.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+//styles
+import styles from "./SearchBar.module.css";
+
+const SearchBar = () => {
+  const [searchData, setSearchData] = useState({
+    min: "",
+    max: "",
+  });
+
+  const router = useRouter();
+
+  const changeHandler = (event) => {
+    setSearchData({
+      ...searchData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const clickHandler = () => {
+    if (searchData.min && searchData.max) {
+      router.push(`fillter/${searchData.min}/${searchData.max}`)
+    } else {
+      alert('Insert min and max price')
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <input
+          placeHolder="Insert min price"
+          name="min"
+          value={searchData.min}
+          onChange={changeHandler}
+        />
+        <input
+          placeHolder="Insert max price"
+          name="max"
+          value={searchData.max}
+          onChange={changeHandler}
+        />
+      </div>
+      <button onClick={clickHandler}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
--- carshope-botocar-project/pages/cars/index.js
+++ carshope-botocar-project/pages/cars/index.js
@@ -1,10 +1,15 @@
-import Categories from '../../components/modules/Categories';
-import Cars from '../../components/templates/Cars';
+//fake data
 import carsData from '../../data/carsData'
+
+//components
+import Categories from '../../components/modules/Categories';
+import SearchBar from '../../components/modules/SearchBar';
+import Cars from '../../components/templates/Cars';
 
 const index = () => {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <Cars data={carsData} />
     </div>
