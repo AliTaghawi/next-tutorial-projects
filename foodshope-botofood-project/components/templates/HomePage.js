dc4da6eb17d1e6963c654884083bf93f63e10fe1
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-19 09:09:25 +0330

    creating Guide.js & Instruction.js & Restrictions.js modules for HomePage temp

--- foodshope-botofood-project/components/templates/HomePage.js
+++ foodshope-botofood-project/components/templates/HomePage.js
@@ -1,6 +1,9 @@
 import Banner from '../modules/HomePage/Banner';
 import Attributes from '../modules/HomePage/Attributes';
 import Definition from '../modules/HomePage/Definition';
+import Instruction from '../modules/HomePage/Instruction';
+import Guide from '../modules/HomePage/Guide';
+import Restrictions from '../modules/HomePage/Restrictions';
 import styles from './HomePage.module.css'
 
 const HomePage = () => {
@@ -9,6 +12,9 @@ const HomePage = () => {
       <Banner />
       <Attributes />
       <Definition />
+      <Instruction />
+      <Guide />
+      <Restrictions />
     </div>
   );
 };
