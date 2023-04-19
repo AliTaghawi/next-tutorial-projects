b6bae618ebad05c93a461eff420bbab64bed83c7
Ali Taghawi <marshaltaghawi@gmail.com>
2023-04-19 08:44:25 +0330

    creating Attributes.js and Definition.js modules for HomePage temp

--- foodshope-botofood-project/components/templates/HomePage.js
+++ foodshope-botofood-project/components/templates/HomePage.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import Banner from '../modules/HomePage/Banner';
+import Attributes from '../modules/HomePage/Attributes';
+import Definition from '../modules/HomePage/Definition';
 import styles from './HomePage.module.css'
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
       <Banner />
+      <Attributes />
+      <Definition />
     </div>
   );
 };
