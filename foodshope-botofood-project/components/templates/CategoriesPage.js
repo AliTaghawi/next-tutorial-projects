a71a3458dcc5fe312f4c9e9be520b925613111e3
Ali Taghawi <marshaltaghawi@gmail.com>
2023-05-10 12:40:22 +0330

    <foodshope project> creating categories page and categoriesPage temp

---
+++ foodshope-botofood-project/components/templates/CategoriesPage.js
@@ -0,0 +1,11 @@
+import styles from './CategoriesPage.module.css'
+
+const CategoriesPage = () => {
+  return (
+    <div>
+      CategoriesPage
+    </div>
+  );
+};
+
+export default CategoriesPage;
