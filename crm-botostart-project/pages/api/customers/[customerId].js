12b33df86da83c899f35972dd569390972f8d8e1
Ali Taghawi <marshaltaghawi@gmail.com>
2023-07-21 03:33:47 +0330

    <CRM project> adding "await" to single customer "GET" api

--- crm-botostart-project/pages/api/customers/[customerId].js
+++ crm-botostart-project/pages/api/customers/[customerId].js
@@ -13,7 +13,7 @@ export default async function handler(req, res)  {
 
   if (req.method === "GET") {
     try {
-      const customer = Customer.findOne({_id: id})
+      const customer = await Customer.findOne({_id: id})
       res.status(200).json({status: "successful", data: customer})
     } catch (err) {
       console.log(err);
