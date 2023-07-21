e3f55547540f5c3cdc3dc02a74bb65a7ec2efb2f
Ali Taghawi <marshaltaghawi@gmail.com>
2023-07-21 03:30:22 +0330

    <CRM project> creating dynamic api "/api/customers/[id]" for single customer; and developing "GET" api for it

---
+++ crm-botostart-project/pages/api/customers/[customerId].js
@@ -0,0 +1,23 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+
+export default async function handler(req, res)  {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({status: "failed", message: "failed to connect DB"})
+  }
+
+  const id = req.query.customerId
+
+  if (req.method === "GET") {
+    try {
+      const customer = Customer.findOne({_id: id})
+      res.status(200).json({status: "successful", data: customer})
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({status: "failed", message: "Internal server error"})
+    }
+  }
+}
