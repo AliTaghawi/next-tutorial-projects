f37d1ed605d2e91ba5a4733a0f9ad0b469a27b75
Ali Taghawi <marshaltaghawi@gmail.com>
2023-07-21 05:16:19 +0330

    <CRM project> developing home page and creating "HomePage.js" template component

--- crm-botostart-project/pages/index.js
+++ crm-botostart-project/pages/index.js
@@ -1,8 +1,26 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
 
-export default function Home() {
-  return (
-    <>
-      <div>Home Page</div>
-    </>
-  )
+//components
+import HomePage from "@/components/templates/HomePage";
+
+export default function Home({ customers }) {
+  return <HomePage customers={customers} />;
+}
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      notFound: true,
+    };
+  }
 }
