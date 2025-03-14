e863359d23be23caa07be89c5e3e9c57230d6115
Ali Taghawi <marshaltaghawi@gmail.com>
2025-03-14 17:30:14 +0330

    Mini CRM: completing customer api

--- mini-crm/pages/api/customers/index.js
+++ mini-crm/pages/api/customers/index.js
@@ -1,25 +1,36 @@
+import Customer from "@/models/Customer";
 import connectDB from "@/utils/connectDB";
 import { customerValidationSchema } from "@/utils/validation";
 
-export default async function handler(req, res)  {
+export default async function handler(req, res) {
   try {
-    await connectDB()
+    await connectDB();
   } catch (error) {
-    console.log(error)
-    return res.status(500)
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 
   if (req.method === "POST") {
-    const data = req.body
+    const data = req.body;
 
     try {
-     await customerValidationSchema.validateAsync(data)
+      await customerValidationSchema.validateAsync(data);
     } catch (error) {
-      console.log(error)
-      return res.status(422).json(error.details[0].message)
+      console.log(error);
+      return res
+        .status(422)
+        .json({ status: "failed", error: error.details[0].message });
     }
 
-    res.json("validation passed")
+    try {
+      const customer = await Customer.create(data);
+      res.status(201).json({
+        status: "success",
+        message: "customer created successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
-  
 }
--- mini-crm/utils/validation.js
+++ mini-crm/utils/validation.js
@@ -6,7 +6,7 @@ const customerValidationSchema = Joi.object({
   email: Joi.string().email().required(),
   phone: Joi.string().regex(/^(\+98|0)?9\d{9}$/),
   address: Joi.string(),
-  postalCode: Joi.number().min(10).max(10),
+  postalCode: Joi.number().min(1000000000).max(9999999999),
   date: Joi.date(),
 })
 
